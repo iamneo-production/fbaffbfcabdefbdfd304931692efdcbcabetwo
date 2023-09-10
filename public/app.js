50dfd43ca70ae0bd8c75c0349089f7d49ce75b47
Iamneo <techadmin@iamneo.ai>
2023-09-10 12:16:23 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -7,6 +7,7 @@ const b0=document.getElementById('0');
 const O_TEXT="O"
 const X_TEXT="X"
 let currentPlayer=X_TEXT
+who.innerHTML=`Player X turn`
 let spaces=Array(9).fill(null)
 
 const startGame=()=>{
@@ -22,18 +23,23 @@ function boxClicked(e){
         spaces[id]=currentPlayer
         e.target.innerText=currentPlayer
     
-        currentPlayer=currentPlayer==X_TEXT?O_TEXT:X_TEXT
-        who.innerHTML=`Player ${currentPlayer} turn`
 
         if(playerHasWon()!=false)
         {
-//     2. Display a winning message in the 'result' element when a player wins.
-            if(currentPlayer=X_TEXT)
-                who.innerHTML=`Player X Won🎉`
-            else    
-                who.innerHTML=`Player O Won🎉`
+//      2. Display a winning message in the 'result' element when a player wins.
+            // if(currentPlayer=X_TEXT)
+            //     who.innerHTML=`Player X Won🎉`
+            // else if(currentPlayer=O_TEXT)   
+            //     who.innerHTML=`Player O Won🎉`
 
+            who.innerHTML=`Player ${currentPlayer} Won🎉`
+            
+//      3. Disable all buttons after a win.
+            currentPlayer=''
         }
+
+        currentPlayer=currentPlayer==X_TEXT?O_TEXT:X_TEXT
+        who.innerHTML=`Player ${currentPlayer} turn`
     }
 }
 
@@ -56,8 +62,6 @@ function playerHasWon(){
         let[a,b,c]=cond
         if(spaces[a] && (spaces[a]==spaces[b] && spaces[a]==spaces[c])){
             return [a,b,c];
-        //3. Disable all buttons after a win.
-            currentPlayer=''
         }
     }
     return false;
@@ -72,26 +76,9 @@ function resetGame(){
     boxes.forEach(btn=>{
         btn.innerText=''
     })
+// Your code to re-enable buttons
     currentPlayer=X_TEXT
     who.innerHTML=''
-    // Your code to re-enable buttons
-    // b0.disabled=false;
 }
 
 startGame();
-
-
-//     // Your code to display the current player's turn
-//     // ...
-
-//     // Your code to handle button and cell interactions
-//     // ...
-// };
-
-//     /*
-//     **Part 2: Reset Function (Add your code here)**
-
-//     1. Implement a new function that resets the game to its initial state.
-//     2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
-//     3. Update the 'result' element to indicate the current player's turn.
-//     4. Re-enable all buttons for a new game.
